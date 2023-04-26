f832f3b5c40da5aff063c9a1fede7a5e89ed4418
Javier Martin Oberholzer <javier.oberholzer.epet20@gmail.com>
2023-04-26 09:57:50 -0300

    Variables cuaderno

---
+++ index.ts
@@ -0,0 +1,18 @@
+let notifications: string[] = [
+    'general',
+    'individual',
+    'retiros',
+    'boletin'
+];
+
+let stateNotifications: string[] = [
+    'visto',
+    'no-visto'
+]
+
+let stateHourNotifications: string[] = [
+    '00:00:00',
+    '01/01/01'
+]
+
+console.log(notifications[2]);
