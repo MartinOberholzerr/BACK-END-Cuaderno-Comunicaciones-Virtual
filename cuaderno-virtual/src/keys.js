85dd744d7cf4a20fd1457e382e86ee962bb9340f
Javier Martin Oberholzer <javier.oberholzer.epet20@gmail.com>
2023-05-12 11:06:46 -0300

    LogIn

---
+++ cuaderno-virtual/src/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongodb: {
+    URI: 'mongodb+srv://chikitomail:marto123@cluster0.hivpuap.mongodb.net/?retryWrites=true&w=majority'
+    
+  }
+};
--- cuaderno-virtual/src/server.js
+++ cuaderno-virtual/src/server.js
@@ -1,21 +1,46 @@
-const express = require ("express");
-const bodyParser = require ( "body-parser");
-const mongoose = require ("mongoose");
+const express = require('express');
+const path = require('path');
+const engine = require('ejs-mate');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+const morgan = require('morgan');
 
-const app = express ();
+// initializations
+const app = express();
+require('./database');
+require('./passport/local-auth');
 
-app.use (bodyParser.urlencoded({ extended: false }));
-app.use (bodyParser. json () );
-    
-    mongoose. connect(
-        "mongodb+srv://chikitomail:<password>@cluster0.hivpuap.mongodb.net/?retryWrites=true&w=majority",
-        { useNewUrlParser: true },
-        (err, res) => {
-         err && console.log ("Error conectando a la bd");
-         app.listen(4000, () => {
-        console.log("servidor corriendo en ala ba http://localhost:4000"); 
-        });
-    }
-)
+// settings
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'))
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
 
+// middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(session({
+  secret: 'mysecretsession',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 
+app.use((req, res, next) => {
+  app.locals.signinMessage = req.flash('signinMessage');
+  app.locals.signupMessage = req.flash('signupMessage');
+  app.locals.user = req.user;
+  console.log(app.locals)
+  next();
+});
+
+// routes
+app.use('/', require('./routes/index'));
+
+// Starting the server
+app.listen(app.get('port'), () => {
+  console.log('server on port', app.get('port'));
+});
